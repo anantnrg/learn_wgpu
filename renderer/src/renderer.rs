b9b2377d02b155e10aa227afcd9e0089a0c3d124
Anant Narayan <anantnrg@gmail.com>
2023-11-08 22:13:56 +0530

    shaders

--- renderer/src/renderer.rs
+++ renderer/src/renderer.rs
@@ -9,6 +9,7 @@ pub struct RendererState {
 	pub queue: wgpu::Queue,
 	pub config: wgpu::SurfaceConfiguration,
 	pub size: winit::dpi::PhysicalSize<u32>,
+	pub render_pipeline: wgpu::RenderPipeline,
 	pub window: Window,
 }
 
@@ -44,13 +45,17 @@ impl RendererState {
 			.await
 			.unwrap();
 
+		let shader = device.create_shader_module(wgpu::include_wgsl!("shaders/shader.wgsl"));
+
 		let surface_caps = surface.get_capabilities(&adapter);
+
 		let surface_format = surface_caps
 			.formats
 			.iter()
 			.copied()
 			.find(|f| f.is_srgb())
 			.unwrap_or(surface_caps.formats[0]);
+
 		let config = wgpu::SurfaceConfiguration {
 			usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
 			format: surface_format,
@@ -62,7 +67,48 @@ impl RendererState {
 		};
 		surface.configure(&device, &config);
 
-		Self { window, surface, device, queue, config, size }
+		let render_pipeline_layout =
+			device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+				label: Some("Render Pipeline Layout"),
+				bind_group_layouts: &[],
+				push_constant_ranges: &[],
+			});
+
+		let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+			label: Some("Render Pipeline"),
+			layout: Some(&render_pipeline_layout),
+			vertex: wgpu::VertexState { module: &shader, entry_point: "vs_main", buffers: &[] },
+			fragment: Some(wgpu::FragmentState {
+				module: &shader,
+				entry_point: "fs_main",
+				targets: &[Some(wgpu::ColorTargetState {
+					format: config.format,
+					blend: Some(wgpu::BlendState {
+						color: wgpu::BlendComponent::REPLACE,
+						alpha: wgpu::BlendComponent::REPLACE,
+					}),
+					write_mask: wgpu::ColorWrites::ALL,
+				})],
+			}),
+			primitive: wgpu::PrimitiveState {
+				topology: wgpu::PrimitiveTopology::TriangleList,
+				strip_index_format: None,
+				front_face: wgpu::FrontFace::Cw,
+				cull_mode: None,
+				polygon_mode: wgpu::PolygonMode::Line,
+				unclipped_depth: false,
+				conservative: false,
+			},
+			depth_stencil: None,
+			multisample: wgpu::MultisampleState {
+				count: 1,
+				mask: !0,
+				alpha_to_coverage_enabled: false,
+			},
+			multiview: None,
+		});
+
+		Self { window, surface, device, queue, config, size, render_pipeline }
 	}
 
 	pub fn window(&self) -> &Window {
